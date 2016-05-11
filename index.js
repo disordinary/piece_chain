4e7224575ecbcc0f863ffdde2dc3cf32da81ec82
Ryan McCarvill <ryanmccarvill@Ryans-MBP.telecom>
2016-05-11 17:06:32 +1200

    added better test suite

---
+++ index.js
@@ -0,0 +1,368 @@
+
+
+// A piece chain is a double linked list of text, it's similar to a piece tree but easier to impliment.
+// It's used to manipulate large chunks of text efficiently and quickly, it also has virtually unlimited undo.
+
+const SPAN_SIZE = 256; //the size of a span
+
+class PieceChain {
+	constructor( str ) {
+		
+		this.length =  0;
+		this.head = new Span( );
+		this.tail = new Span( );
+		this._modifyBuffer = [ ];
+		if( typeof str === 'string' ) {
+			this.length = str.length
+			let pieceChain = this._string_into_spans( str );
+
+			this.head.next = pieceChain.startSpan;
+			pieceChain.startSpan.previous = this.head;
+
+			this.tail.previous = pieceChain.endSpan;
+			pieceChain.endSpan.next = this.tail;
+		} else {
+			this.head.next = this.tail;
+			this.tail.previous = this.head;
+		}
+	}
+
+
+	insert( offset , str ) {
+
+		
+		this.length += str.length;
+
+		let { span , cursor } = this._seek( offset ) ;
+
+		this._modifyBuffer.push( span );
+
+		let newSpans = this._string_into_spans( str );
+		let newStartSpan = newSpans.startSpan;
+		let newEndSpan = newSpans.endSpan;
+
+		if( offset === 0 ) {
+			this._joinSpans( newEndSpan , this.head.next );
+			this._joinSpans( this.head , newStartSpan );
+			return;
+		}
+		if( offset >= this.length ) {
+			this._joinSpans( this.tail.previous  , newStartSpan );
+			this._joinSpans( newEndSpan , this.tail );
+			return;
+		}
+
+		
+		
+		let oldSpans = this._split( span , cursor );
+		let oldStartSpan = oldSpans.startSpan;
+		let oldEndSpan = oldSpans.endSpan;
+ 
+		//replace the old span
+		oldStartSpan.previous.next = oldStartSpan;
+		oldEndSpan.next.previous = oldEndSpan;
+
+		//insert the new spans into the chain
+		this._joinSpans( oldStartSpan , newEndSpan );
+		this._joinSpans( newStartSpan , oldEndSpan );
+	}
+
+	del( offset , length ) {
+		this.length -= length;
+		let {span , cursor} = this._seek( offset );
+	
+		if( span.length - cursor > length ) {
+			let spanStart = this._split( span , offset );
+			let spanEnd = this._split( spanStart.endSpan , length );
+			this._joinSpans( span.previous , spanStart.startSpan );
+			this._joinSpans( spanEnd.endSpan , span.next  );
+			this._joinSpans( spanStart.startSpan , spanEnd.endSpan );
+			return;
+		} 
+
+
+			let spanStart = this._split( span , offset );
+			let theEndSpan = this._seek( offset + length )
+			let spanEnd = this._split( theEndSpan.span , theEndSpan.cursor );
+
+			spanStart.startSpan._str = ">>>" + spanStart.startSpan._str ;
+		spanEnd.endSpan._str = "<<<" + spanEnd.endSpan._str ;
+
+			this._joinSpans( span.previous , spanStart.startSpan );
+			this._joinSpans( spanEnd.endSpan , span.next  );
+			this._joinSpans( spanStart.startSpan , spanEnd.endSpan );
+		return;
+		/*let spanStart = this._split( span , offset );
+		let theEndSpan = this._seek( offset + length )
+		let spanEnd = this._split( theEndSpan.span , theEndSpan.cursor );
+		spanStart.startSpan._str = ">>>" + spanStart.startSpan._str ;
+		spanEnd.endSpan._str = "<<<" + spanEnd.endSpan._str ;
+		this._joinSpans( span.previous , spanStart.startSpan );
+		this._joinSpans( spanEnd.endSpan , span.next );
+
+		this._joinSpans( spanStart.startSpan , spanEnd.endSpan );*/
+		
+		
+
+
+		//if( startSpan.span === endSpan.span )
+	}
+
+	toString( ) {
+		var piece = this.head;
+
+			var string = "";
+
+			while( piece.next ) {
+				string += ( piece.str || "" );
+				piece = piece.next;
+			}
+
+			return string;
+	}
+
+	_joinSpans( startSpan , endSpan ) {
+		startSpan.next = endSpan;
+		endSpan.previous = startSpan;
+	}
+
+
+	_string_into_spans( str  ) {
+		let startSpan;
+		let endSpan;
+		if( str.length < SPAN_SIZE ) {
+
+			let startSpan = endSpan = new Span( str );
+
+			return { startSpan , endSpan};
+		} 
+		for( var i = 0 ; i < str.length; i+= SPAN_SIZE ) {
+			let span = new Span( str.slice( i , i+SPAN_SIZE ) );
+			
+			if( endSpan ) {
+				span.previous = endSpan;
+				endSpan.next = span;
+			} else {
+				startSpan = span;
+			}
+			endSpan = span;
+		}
+		return { startSpan , endSpan}; //the last span in a mini chain
+	}
+
+	_split( span , offset ) {
+		
+		let startSpan = new Span( span.str.slice( 0 , offset ) , span.previous  );
+		let endSpan = new Span( span.str.slice( offset ) , startSpan , span.next);
+		startSpan.next = endSpan;
+		return{ startSpan , endSpan };
+	}
+
+	_clone( span ) {
+		return new Span( span.content , span.previous , span.next );
+	}
+
+	createSpan( str ) {
+		return new Span( str );
+	}
+
+	_seek( offset ) {
+		let combinedOffset = 0;
+		let listSpan = this.head;
+
+		
+		while( listSpan.next && offset > combinedOffset + listSpan.length ) {
+			combinedOffset += listSpan.length;
+			listSpan = listSpan.next;
+		}
+
+		return {
+			  span : listSpan
+			, cursor : offset - combinedOffset
+		}
+
+	}
+
+	get _spanSize( ) {
+		//return the span size for testing;
+		return SPAN_SIZE;
+	}
+
+}
+
+class Span {
+	constructor( str , previous , next ) {
+		this.previous = previous;
+		this.next = next;
+		this.str = str;
+	}
+	get next( ) {
+		return this._next;
+	}
+
+	set next( value ) {
+		this._next = value;
+	}
+
+	get previous( ) {
+		return this._previous;
+	}
+
+	set previous( value ) {
+		this._previous = value;
+	}
+
+	get str() {
+		return this._str
+	}
+
+	set str( value ) {
+		
+		this._str = value || "";
+		this.length = this._str.length;
+
+	}
+}
+
+module.exports = PieceChain;
+/*
+class PieceChain {
+	constructor( str ) {
+		this.head = new Span( "" , null , null );
+		this.tail = new Span( "" , this.head , null );
+		this.head.next = this.tail;
+		this.length = 0;
+		this._modifyBuffer = [ ]; //the changes
+		this._redoBuffer = [ ]; //not implemented
+		this._lastAaddedSpan; //not implemented, the last added span 
+	}
+
+	insert( offset , str ) {
+		let { span , cursor } = this._seek( offset ) ;
+		let { newStartSpan , newEndSpan } = this._string_into_spans( str );
+		this._modifyBuffer.push( span );
+
+		if( cursor === 0 ) {
+			//head
+			let newSpan = this._clone( span );
+			newStartSpan.previous = newSpan.previous;
+			newEndSpan.next = newSpan;
+		} else if( cursor === span.length ) {
+			//tail
+			let newSpan = this._clone( span );
+			newStartSpan.previous = newSpan;
+			newEndSpan.next = newSpan.next;
+
+		} else {
+			
+			let { startSpan , endSpan } = this._split( span , cursor );
+			endSpan.previous = newSpans;
+			while( newSpans.previous ) {
+				newSpans = newSpans.previous;
+			}
+			startSpan = newSpans;
+		}
+	}
+
+	delete( offset , length ) {
+		
+		let { startSpan , startCursor } = this._seek( offset );
+		let { endSpan , endCursor } = this._seek( offset + length ); //can speed up by refactoring seek so that it's a function on the sapn
+		
+		this._modifyBuffer.push( { startSpan , endSpan } );
+
+		let newStartSpan = this._split( startSpan , startCursor );
+		let newEndSpan = this._split( endSpan , endCursor );
+		startSpan.previous = newStartSpan;
+		endSpan.next = newEndSpan;
+	}
+
+
+	_split( span , offset ) {
+		let startSpan = new Span( span.content.subString( 0 , offset , span.previous ) );
+		let endSpan = new Span( span.content.subString( offset ) , startSpan , span.next);
+		startSpan.next = endSpan;
+		return{ startSpan , endSpan };
+	}
+	_clone( span ) {
+		return new Span( span.content , span.previous , span.next );
+	}
+	_string_into_spans( str  ) {
+		let startSpan;
+		let endSpan;
+		for( var i = 0 ; i < str.length; i+= SPAN_SIZE ) {
+			let span = new Span( str.subString( i , SPAN_SIZE ) );
+
+			if( endSpan ) {
+				span.previous = endSpan;
+				endSpan.next = span;
+			} else {
+				startSpan = span;
+			}
+			endSpan = span;
+		}
+
+		return { startSpan , endSpan}; //the last span in a mini chain
+	}
+
+
+	_seek( offset ) {
+		let combinedOffset = 0;
+		let listSpan = this.head;
+		while( offset > combinedOffset + listSpan.length ) {
+			combinedOffset += listSpan.length;
+			listSpan.next();
+		}
+
+		return {
+			  span : listSpan
+			, cursor : offset - ( combinedOffset - listSpan.length )
+		}
+
+	}
+
+}
+
+class Span {
+	constructor( str  , previous , next) {
+		this.offset = 0;
+		this.next = next;
+		this.previous = previous;
+		this._content = "";
+		this.length = 0;
+	}
+	get previous( ) {
+		return this._previous;
+	}
+
+	set previous( value ) {
+		if( !value ) return;
+		if( value.next !== this ) {
+			value.next = this;
+		}
+		this._previous = value;
+	}
+
+	get next( ) {
+		return this._next;
+	}
+
+	set next( value ) {
+		if( !value ) return;
+		if( value.previous !== this ) {
+			value.previous = this;
+		}
+		this._next = value;
+	}
+
+	get content() {
+		return this._content
+	}
+
+	set content( value ) {
+		this._content = value;
+		this.length = value.length;
+	}
+}
+
+module.exports = PieceChain;*/
---
+++ test/test.js
@@ -0,0 +1,186 @@
+var assert = require("assert");
+var PieceChain = require("../index.js");
+ 
+ function stringSplice( str , start , delcount , newstr ) {
+ 	newstr = newstr || "";
+ 	return str.slice( 0 , start ) + newstr + str.slice( start + delcount );
+ }
+
+var test_string = "";
+var lengthOfTestString = 1000;
+for( var i = 0; i < lengthOfTestString; i++ ) {
+	test_string+= i%10;
+}
+
+
+
+describe( "PieceChain insertion tests" , ( ) => {
+	
+		
+		it('The string from the chain should be the same if built from the back or from the start', ( done ) => {
+
+			var pc = new PieceChain( test_string );
+			var piece = pc.head;
+
+			var movingForwards = "";
+
+			while( piece.next ) {
+				movingForwards += ( piece.str || "" );
+				piece = piece.next;
+			}
+
+			
+			var piece = pc.tail;
+
+			var movingBackwards = "";
+
+			while( piece.previous ) {
+				movingBackwards = ( piece.str || "" ) + movingBackwards;
+				piece = piece.previous;
+			}
+
+			
+			assert.equal( movingBackwards , movingForwards );
+			done();
+		} );
+
+		it( 'Splitting a span should result in two spans at the same split point ' , ( done ) => {
+			var pc = new PieceChain( );
+			var spans = pc._split( pc.createSpan( "TESTSTR") , 3);
+
+			assert.equal( spans.startSpan.str , "TES" );
+			assert.equal( spans.endSpan.str , "TSTR" );
+			
+			assert.equal(spans.startSpan.next , spans.endSpan );
+			assert.equal(spans.endSpan.previous , spans.startSpan );
+			done();
+			
+		});
+
+		it( 'A seek operation should return a string and cursor offset' , ( done ) => {
+				var pc = new PieceChain( test_string );
+				var offset = 588; //the offset to seek to
+				var seeked = pc._seek( offset );
+				var spanSize = pc._spanSize;
+				var offsetForSpan  = Math.floor( offset / spanSize) * spanSize;
+
+				assert.equal( seeked.span.str , test_string.slice( offsetForSpan , offsetForSpan + spanSize ));// Math.floor( offset / 256 ) * 256 );
+				assert.equal( seeked.cursor,offset % spanSize );
+				done();
+		});
+		
+		it( 'I can add to the start and end of the chain ' , ( done ) => {
+			var pc = new PieceChain( test_string );
+			var newTestString = test_string;
+
+			pc.insert( lengthOfTestString , "End");
+			newTestString += "End";
+
+
+			pc.insert( lengthOfTestString + 100, " BEYOND THE END");
+			newTestString += " BEYOND THE END";
+
+			pc.insert( 0 , "Start" );
+			newTestString = "Start" + newTestString;
+			
+			pc.insert( 100 , "Middle" );
+			newTestString = stringSplice( newTestString , 100 , 0 , "Middle");
+		
+			assert.equal( pc.toString( ) , newTestString );
+			done();
+		});
+
+		it( 'I can add to the start of a piece ' , ( done ) => {
+			var pc = new PieceChain( test_string );
+			var newTestString = test_string;
+
+			pc.insert( 257 , "**************************" );
+		
+
+			
+			newTestString = stringSplice( newTestString , 257 , 0 , "**************************");
+			
+			assert.equal( pc.toString() , newTestString );
+			done();
+		});
+
+		it( 'I can add to the end of a piece ' , ( done ) => {
+			var pc = new PieceChain( test_string );
+			var newTestString = test_string;
+
+			pc.insert( 256 , "**************************" );
+			
+			newTestString = stringSplice( newTestString , 256 , 0 , "**************************");
+		
+			assert.equal( pc.toString() , newTestString );
+			done();
+		});
+
+		it( 'The length reflects the actual length' , ( done ) => {
+			
+			var pc = new PieceChain( test_string );
+			var newTestString = test_string;
+
+			pc.insert( 256 , "**************************" );	
+			newTestString = stringSplice( newTestString , 256 , 0 , "**************************");
+
+			pc.insert( 1256 , "**************************" );	
+			newTestString = stringSplice( newTestString , 1256 , 0 , "**************************");
+		
+			assert.equal( pc.length , newTestString.length );
+
+			done();
+		});
+
+});
+
+describe( "PieceChain deletion tests" , ( ) => {
+	it( 'I can delete within one piece' , ( done ) => {
+		var pc = new PieceChain( test_string );
+		var delTestString = test_string;
+		pc.del( 100 , 4 );
+		delTestString = stringSplice(delTestString , 100 , 4 );
+
+		
+		assert.equal( pc.toString( ) , delTestString );
+		assert.equal( pc.length , delTestString.length );
+		done();
+	});
+
+	it( 'I can delete multiple pieces' , ( done ) => {
+		var pc = new PieceChain( test_string );
+		var delTestString = test_string;
+		pc.del( 150 , 300 );
+		delTestString = stringSplice(delTestString , 150 , 300 );
+		assert.equal( pc.toString( ) , delTestString );
+		assert.equal( pc.length , delTestString.length );
+		done();
+	});
+});
+
+
+/*describe( "PieceChain benchmarking" , ( ) => {
+	var pc = new PieceChain( test_string );
+	var benchMarkString = test_string;
+	it( 'The PieceChain should be faster at insertions than a string' , ( done ) => {
+		var stringStartTime = Date.now();
+		for( var i = 0; i < 1000; i++ ) {
+			benchMarkString = stringSplice( benchMarkString , Math.random() * lengthOfTestString , 0 , "TEST" );
+		}
+
+		var stringTime = Date.now( ) - stringStartTime ;
+
+
+		var pcStartTime = Date.now();
+		for( var i = 0; i < 1000; i++ ) {
+			pc.insert( Math.random() * lengthOfTestString , "TEST" );
+		//	benchMarkString = stringSplice( benchMarkString , Math.random() * lengthOfTestString , 0 , "TEST" );
+		}
+
+		var pcTime = Date.now( ) - pcStartTime ;
+		assert.ok( pcTime < stringTime );
+		console.log( "The PieceChain did it in: " + pcTime + "ms, String manipulation did it in: " + stringTime +"ms" );
+		done();
+	} );
+} );*/
+
